590689a81d2dd1b83c446a926542abb603a4af2f
Juan Aleman <juan.j.aleman@gmail.com>
2019-05-26 18:05:03 -0500

    Worked on the mobile responsiveness for the Nav Bar

--- components/index.js
+++ components/index.js
@@ -6,3 +6,13 @@ window.open("https://www.apple.com/ios/app-store/");
   function openGoogle() {
   window.open("https://play.google.com/store");
 }
+
+// NavBar Javascript
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
